7962b5537dd809c7acbe0302e448ed64f3752091
mahmoudsemhan <mahmoudsemhan9@gmail.com>
2024-07-02 17:08:05 +0300

    add file of project

---
+++ MyShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Web.Data;
+using MyShop.Web.Models;
+
+namespace MyShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbConext _context;
+
+        public CategoryController(ApplicationDbConext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) NotFound();
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+
+    }
+}
---
+++ MyShop.Web/Data/ApplicationDbConext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Web.Models;
+
+namespace MyShop.Web.Data
+{
+    public class ApplicationDbConext:DbContext
+    {
+        public ApplicationDbConext(DbContextOptions<ApplicationDbConext> options):
+            base(options)
+        {
+      
+        }
+        public DbSet<Category> Categories  { get; set; }
+    }
+}
